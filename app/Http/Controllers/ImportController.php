0dd5bf8b128767d906000e357c8ec3321768c73a
highfrezh <highfrezh14@gmail.com>
2022-11-04 09:12:57 +0100

    filename.output.csv created

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -21,25 +21,47 @@ class ImportController extends Controller
                 }
             }
 
-
+            //getting the uploaded file
             $uploadedFile = public_path('/imports/csv/'.$filename);
+
             //csvToArray() method convert csv file to array check down below,         
             $nfts = $this->csvToArray($uploadedFile);
+
+            //creating csv file and json format or each nfts
+            $fp = fopen('filename.output.csv', 'w');
             foreach($nfts as $nft){
+
                 // generating sha256 code
                 $hash = hash_hmac('sha256', utf8_encode($nft['Filename']), rand(), false);
+
                 //appending Hash data to the array
                 $nft['Hash'] = $hash;
+
                 //converting attribute to array
                 $nft['Attributes'] = array_column(array_chunk(preg_split("/[:;]/", $nft['Attributes']), 2), 1, 0);
-                //creating json file for each row
-                $fileName = "nft".$nft['Filename'].'.json';
-                $fileStorePath = public_path('/imports/nfts/'.$fileName);
-                
-                File::put($fileStorePath, json_encode($nft));    
-            }
 
-            return "NFTs JSON files created successfully!";
+                // creating json file for each row
+                $fileName = "nft".$nft['Filename'].'.json';
+                $fileStorePath = public_path('/imports/nfts/'.$fileName);                
+                File::put($fileStorePath, json_encode($nft));
+                
+                // array to string conversion
+                $flattened = $nft['Attributes'];
+                array_walk($flattened, function(&$value, $key) {
+                    $value = "{$key}:{$value}";
+                });
+                $nft['Attributes'] = implode('; ', $flattened);
+
+                //creating the new csv file (filename.output.csv)
+                    fputcsv($fp, $nft);        
+            }
+            fclose($fp);
+            
+            return response()->json([
+                "message" => "nft json and filename.output.csv file created successfully!",
+                "nfts path" => "public/imports/nfts",
+                "filename.output.csv path" => "public",
+            ]);
 
         }else{
             //rendering a view blade file to import csv file;
